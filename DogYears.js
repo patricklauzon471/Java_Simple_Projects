ccb100d499ccbac3b4f9f2b3b7d7eecae15277ac
patricklauzon471 <83890368+patricklauzon471@users.noreply.github.com>
2022-02-13 08:17:19 -0500

    Add files via upload

---
+++ DogYears.js
@@ -0,0 +1,10 @@
+const myAge = 20;
+let earlyYears = 2;
+earlyYears *= 10.5;
+laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+myAgeInDogYears = earlyYears + laterYears;
+myName = 'Patrick Lauzon'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
